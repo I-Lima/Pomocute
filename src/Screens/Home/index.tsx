506d3edb0c6d71f7288e3d42ebef03e4ed0d3578
Ingrid Lima <ibezerradelima@gmail.com>
2023-04-24 17:14:42 -0300

    :recycle: refactor: Add function switch button to play when reset

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,48 +1,45 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useTimer } from "../../Utils/Timer";
 import { View } from "react-native";
 import { Dimension, } from "../../Constants/Styles";
 import ButtonPlay from "../../Components/ButtonPlay";
 import Timer from "../../Components/Timer";
+import ButtonRefresh from "../../Components/ButtonRefresh";
+
+interface ButtonPlayRef {
+  changeTypeToPlay: () => void;
+}
 
 function Home() {
-  const { formattedTime, startTimer, pauseTimer, resetTimer } = useTimer(200);
+  const { formattedTime, startTimer, pauseTimer, resetTimer } = useTimer(10);
   const [isRunning, setIsRunning] = useState(false);
   const widthHome = isRunning ? {width: Dimension.WIDTH - 50} : null;
+  const buttonPlayRef = useRef<ButtonPlayRef>(null);
 
   const handlePlay = () => {
     setIsRunning(true);
-
     startTimer();
   };
 
   const handleRefresh = () => {
-    setIsRunning(false);
-
     resetTimer();
+
+    if(buttonPlayRef.current)
+      buttonPlayRef.current.changeTypeToPlay();
+
+    setIsRunning(false);
   };
 
   return(
-    <View style={{
-      alignItems: 'center',
-      justifyContent: 'center',
-      flex: 1,
-    }}>
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1}}>
       <Timer time={formattedTime} color="yellow" />
 
-      <View style={{
-        height: Dimension.HEIGHT / 6
-      }} />
+      <View style={{ height: Dimension.HEIGHT / 6 }} />
 
-      <View style={[{
-        flexDirection: 'row',
-        justifyContent:'space-between',
-      }, widthHome]}>
-        <ButtonPlay type="play" color="yellow" onPressPlay={handlePlay} onPressPause={pauseTimer} />
+      <View style={[{ flexDirection: 'row', justifyContent:'space-between' }, widthHome]}>
+        <ButtonPlay color="yellow" onPressPlay={handlePlay} onPressPause={pauseTimer} ref={buttonPlayRef} />
 
-        {isRunning && (
-          <ButtonPlay type="refresh" color="yellow" onPressRefresh={handleRefresh} />
-        )}
+        {isRunning && <ButtonRefresh color="yellow" onPressRefresh={handleRefresh} /> }
       </View>
     </View>
   );
