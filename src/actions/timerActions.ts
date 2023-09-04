604f2a5228257de5fdac2dea1500c3ff448794b2
Ingrid Lima <ibezerradelima@gmail.com>
2023-09-04 07:34:25 -0300

    adapt to save time values in seconds
    #24

--- src/actions/timerActions.ts
+++ src/actions/timerActions.ts
@@ -2,15 +2,15 @@ import { UPDATE_BIGGER_REST_TIMER, UPDATE_FOCUS_TIMER, UPDATE_REST_TIMER } from 
 
  export const changeFocusTimer = (value: string) => ({
   type: UPDATE_FOCUS_TIMER,
-  payload: {value: Number(value)}
+  payload: {value: Number(value)*60}
  });
 
  export const changeRestTimer = (value: string) => ({
   type: UPDATE_REST_TIMER,
-  payload: {value: Number(value)}
+  payload: {value: Number(value)*60}
  });
 
- export const changeBiggerRestTiemr = (value: string) => ({
+ export const changeBiggerRestTimer = (value: string) => ({
   type: UPDATE_BIGGER_REST_TIMER,
-  payload: {value: Number(value)}
+  payload: {value: Number(value)*60}
  });
--- src/reducers/timerReducer.ts
+++ src/reducers/timerReducer.ts
@@ -1,9 +1,10 @@
 import { UPDATE_BIGGER_REST_TIMER, UPDATE_FOCUS_TIMER, UPDATE_REST_TIMER } from "../Constants/Reducer"
+import { timerStateProps } from "../Screens/Configure";
 
-const INITIAL_STATE = {
-  focusTimer: 25,
-  restTimer: 5,
-  biggerRestTimer: 10,
+const INITIAL_STATE: timerStateProps = {
+  focusTimer: 1500,
+  restTimer: 300,
+  biggerRestTimer: 600,
 }
 
 const timerReducer = (state = INITIAL_STATE, action: any) => {
