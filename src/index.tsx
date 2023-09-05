e8a6b18364fc482819caf84bb680815d0b8c5df3
Ingrid Lima <ibezerradelima@gmail.com>
2023-09-05 15:58:46 -0300

    Get reducer on asyncStorage
    #36

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,31 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import appReducer from "./reducers";
 import { createStore } from "redux";
 import Routes from "./routes";
-
-const store = createStore(appReducer);
+import { loadColorStateFromAsyncStorage } from "./reducers/colorReducer";
 
 const Index = () => {
+  const [preloadStates, setPreloadStates] = useState({});
+
+  useEffect(() => {
+    const loadAsyncData = async () => {
+      try {
+        const data = await loadColorStateFromAsyncStorage();
+        setPreloadStates({color: data});
+      } catch (error) {return; }
+    };
+
+    loadAsyncData();
+  }, []);
+
+  const store = createStore(appReducer, preloadStates);
+
   return (
     <Provider store={store}>
       <Routes />
     </Provider>
-  )
+  );
 };
 
 export default Index;
